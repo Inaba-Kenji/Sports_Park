059860d3e524530d0dd56ae88a0fac49b0ee0358
EC2 Default User <ec2-user@ip-172-31-35-60.ap-northeast-1.compute.internal>
2020-11-11 15:51:21 +0000

    [create] signup login

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,20 @@
+class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:postal_code,:address])
+  end
+
+  # sign_in後の遷移
+  def after_sign_in_path_for(resource_or_scope)
+    root_path
+  end
+
+  # sign_out後の遷移
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
+
+end
