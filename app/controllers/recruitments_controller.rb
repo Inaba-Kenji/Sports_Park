c44643fbfdc6f73d7c856ffafbdf33392a5e8779
EC2 Default User <ec2-user@ip-172-31-30-16.ec2.internal>
2020-11-21 15:14:46 +0000

    [fix] function-dm

--- app/controllers/recruitments_controller.rb
+++ app/controllers/recruitments_controller.rb
@@ -13,12 +13,11 @@ class RecruitmentsController < ApplicationController
 
     # DM機能
     @userId = @recruitment.user.id
-    #自分が参加しているメッセージルーム情報を取得する
+
     @currentUserEntry = Entry.where(user_id: current_user.id)
-    #選択したユーザのメッセージルーム情報を取得する
+
     @userEntry = Entry.where(user_id: @userId)
 
-    #current_userと選択したユーザ間に共通のメッセージルームが存在すればフラグを立てる
     unless @userId == current_user.id
       @currentUserEntry.each do |cu|
         @userEntry.each do |u|
@@ -28,14 +27,12 @@ class RecruitmentsController < ApplicationController
           end
         end
       end
-      #無ければ作る
+
       unless @isRoom
         @room = Room.new
         @entry = Entry.new
       end
     end
-
-
   end
 
   def create
