a59cc42a106b4ca8e56ebd05d8406cdebaa8627f
EC2 Default User <ec2-user@ip-172-31-30-16.ec2.internal>
2020-12-14 02:07:08 +0000

    [start] notification

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_04_021914) do
+ActiveRecord::Schema.define(version: 2020_12_14_020313) do
 
   create_table "entries", force: :cascade do |t|
     t.integer "user_id"
@@ -32,6 +32,16 @@ ActiveRecord::Schema.define(version: 2020_12_04_021914) do
     t.integer "user_id"
     t.integer "room_id"
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.integer "visiter_id"
+    t.integer "visited_id"
+    t.integer "recruitment_id"
+    t.string "action"
+    t.boolean "checked", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
